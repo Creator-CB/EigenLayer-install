284d58fd24947c3388102e17e343f0d29c0cec94
Creator-CB <117445826+Creator-CB@users.noreply.github.com>
2024-02-01 04:03:18 +0200

    Update EigenLayer.sh

--- EigenLayer.sh
+++ EigenLayer.sh
@@ -16,78 +16,65 @@ function line {
 }
 
 function install_go {
-  # Update and upgrade the system
-  sudo apt-get update -y && sudo apt-get upgrade -y
+    sudo apt-get update -y && sudo apt-get upgrade -y
 
-  # Download and extract Go
-  wget https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
-  sudo tar -C $HOME/ -xzf go1.13.5.linux-amd64.tar.gz
+    wget https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
+    sudo tar -C /usr/local/ -xzf go1.13.5.linux-amd64.tar.gz
+    cd /usr/local/
+    echo $PATH
 
-  # Update environment variables
-  echo 'export PATH=$PATH:$HOME/go/bin' >> $HOME/.profile
-  echo 'export PATH=$PATH:$HOME/bin' >> $HOME/.profile
+    echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a $HOME/.profile
+    echo 'export PATH=$PATH:~/bin' | sudo tee -a $HOME/.profile
 
-  source $HOME/.profile
+    source $HOME/.profile
 
-  # Install Golang
-  sudo apt-get install -y golang-go
-  go version
+    apt install golang-go
+    go version
 }
 
 function install_docker {
-  # Install Docker dependencies
-  sudo apt-get update
-  sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl gnupg
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-  # Add Docker GPG key and repository
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
 
-  # Install Docker
-  sudo apt-get update
-  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-  # Add the user to the docker group
-  sudo groupadd docker
-  sudo usermod -aG docker $USER
-  newgrp docker
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+    newgrp docker
 
-  # Test Docker installation
-  docker run hello-world
-}
-
-function eigenda_setup {
-  # Clone Eigenda setup repository
-  cd $HOME
-  git clone https://github.com/Layr-Labs/eigenda-operator-setup.git
-  cd eigenda-operator-setup
-
-  # Copy environment file
-  cp .env.example .env
-
-  cd $HOME
+    docker run hello-world
 }
 
 function install_eigenlayer {
-  # Install Eigenlayer CLI
-  cd $HOME
-  curl -sSfL https://raw.githubusercontent.com/layr-labs/eigenlayer-cli/master/scripts/install.sh | sh -s
-  source $HOME/.profile  # Ensure the changes take effect in the current session
-  eigenlayer version  # Check Eigenlayer version
+    cd ~
+    curl -sSfL https://raw.githubusercontent.com/layr-labs/eigenlayer-cli/master/scripts/install.sh | sh -s
+    export PATH=$PATH:~/bin
+
+}
+
+function eigenda_setup {
+    cd ~
+    git clone https://github.com/Layr-Labs/eigenda-operator-setup.git
+    cd eigenda-operator-setup
+    cp .env.example .env
 }
 
 function main {
-  colors
-  logo
-  install_go
-  install_docker
-  eigenda_setup
-  install_eigenlayer 
+    colors
+    logo
+    install_go
+    install_docker
+    install_eigenlayer
+    eigenda_setup
 }
 
 main
